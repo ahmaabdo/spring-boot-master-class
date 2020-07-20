1269eb968b5c4f6ee97fa19169c926bbb3e43cc1
Ahmad Abdo <ahmed.abdel-gaied@appspro-me.com>
2020-07-20 05:46:01 +0200

    Add initBinder method

--- spring-boot-2-todo-sample/src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
+++ spring-boot-2-todo-sample/src/main/java/com/in28minutes/springboot/web/controller/TodoController.java
@@ -1,10 +1,13 @@
 package com.in28minutes.springboot.web.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.validation.Valid;
-
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -23,6 +26,13 @@ public class TodoController {
 
 	@Autowired
 	TodoService service;
+
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(
+				dateFormat, false));
+	}
 
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showTodos(ModelMap model) {
@@ -64,6 +74,7 @@ public class TodoController {
 
 		return "redirect:/list-todos";
 	}
+
 
 	@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
 	public String addTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
